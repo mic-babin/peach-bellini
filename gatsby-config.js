92eab0016844f939b903e2609f8347f679504b83
Michael Babin <info@micb.dev>
2023-03-16 06:48:59 -0400

    Trying

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-preload-fonts`,
+    // `gatsby-plugin-preload-fonts`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
